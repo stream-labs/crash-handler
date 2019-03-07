8b7c37ca9ee0a30740436b618b6a1a2f807bfdb4
Eddy Gharbi <eddy.gharbi@streamlabs.com>
2019-03-07 14:29:15 -0800

    Revert "Remove the native module"
    This reverts commit c946f22953c98e839277b70b694538385fe70c1c.

---
+++ crash-handler-module/module.cpp
@@ -0,0 +1,10 @@
+#include <node.h>
+
+using namespace v8;
+
+void init(Local<Object> exports) {
+    /// Functions ///
+    // NODE_SET_METHOD(exports, "startHook", StartHotkeyThreadJS);
+}
+
+NODE_MODULE(crash_handler, init)
