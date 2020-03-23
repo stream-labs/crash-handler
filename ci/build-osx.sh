2a2534e0f3eaca4cc1482ec8df2e5117cdb17726
EddyGharbi <eddygharbiema@gmail.com>
2020-03-23 14:18:37 -0700

    Fix typo

--- ci/build-osx.sh
+++ ci/build-osx.sh
@@ -1,3 +1,3 @@
-cmake $SLBuildDirectory} -DCMAKE_INSTALL_PREFIX=${FullDistributePath}/crash-handler -DNODEJS_NAME=${RuntimeName} -DNODEJS_URL=${RuntimeURL} -DNODEJS_VERSION=${RuntimeVersion} -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake ${SLBuildDirectory} -DCMAKE_INSTALL_PREFIX=${FullDistributePath}/crash-handler -DNODEJS_NAME=${RuntimeName} -DNODEJS_URL=${RuntimeURL} -DNODEJS_VERSION=${RuntimeVersion} -DCMAKE_BUILD_TYPE=RelWithDebInfo
 
 cmake --build ${SLBuildDirectory} --target install --config RelWithDebInfo
