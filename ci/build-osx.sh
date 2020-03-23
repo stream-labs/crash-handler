c4bb8aafa5348f8e1c8b67ac773b5382402d261a
EddyGharbi <eddygharbiema@gmail.com>
2020-03-23 14:41:47 -0700

    Fix

--- ci/build-osx.sh
+++ ci/build-osx.sh
@@ -1,8 +1,4 @@
 mkdir ${SLBuildDirectory}
-cd ${SLBuildDirectory}
-
-cmake .. -DCMAKE_INSTALL_PREFIX=${FullDistributePath}/crash-handler -DNODEJS_NAME=${RuntimeName} -DNODEJS_URL=${RuntimeURL} -DNODEJS_VERSION=${RuntimeVersion} -DCMAKE_BUILD_TYPE=RelWithDebInfo
-
-cd ..
+cmake ${SLBuildDirectory} -DCMAKE_INSTALL_PREFIX=${FullDistributePath}/crash-handler -DNODEJS_NAME=${RuntimeName} -DNODEJS_URL=${RuntimeURL} -DNODEJS_VERSION=${RuntimeVersion} -DCMAKE_BUILD_TYPE=RelWithDebInfo
 
 cmake --build ${SLBuildDirectory} --target install --config RelWithDebInfo
