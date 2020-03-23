e58855135ddf4794f48bb7bae2d1f4e86423c6b0
EddyGharbi <eddygharbiema@gmail.com>
2020-03-23 14:57:04 -0700

    Fix dist path

--- ci/build-osx.sh
+++ ci/build-osx.sh
@@ -3,7 +3,7 @@ cd build
 
 mkdir crash-handler
 
-cmake .. -DCMAKE_INSTALL_PREFIX=${PWD}/build/distribute/crash-handler -DNODEJS_NAME=${RuntimeName} -DNODEJS_URL=${RuntimeURL} -DNODEJS_VERSION=${RuntimeVersion} -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake .. -DCMAKE_INSTALL_PREFIX=${PWD}/distribute/crash-handler -DNODEJS_NAME=${RuntimeName} -DNODEJS_URL=${RuntimeURL} -DNODEJS_VERSION=${RuntimeVersion} -DCMAKE_BUILD_TYPE=RelWithDebInfo
 
 cd ..
 
