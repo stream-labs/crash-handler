c94b0888295760e00c178f4ea91a61fb57e608fa
EddyGharbi <eddygharbiema@gmail.com>
2020-03-23 14:31:07 -0700

    Attempt to fix cmake issues

--- ci/build-osx.sh
+++ ci/build-osx.sh
@@ -1,3 +1,8 @@
-cmake ${SLBuildDirectory} -DCMAKE_INSTALL_PREFIX=${FullDistributePath}/crash-handler -DNODEJS_NAME=${RuntimeName} -DNODEJS_URL=${RuntimeURL} -DNODEJS_VERSION=${RuntimeVersion} -DCMAKE_BUILD_TYPE=RelWithDebInfo
+mkdir ${SLBuildDirectory}
+cd ${SLBuildDirectory}
+
+cmake .. -DCMAKE_INSTALL_PREFIX=${FullDistributePath}/crash-handler -DNODEJS_NAME=${RuntimeName} -DNODEJS_URL=${RuntimeURL} -DNODEJS_VERSION=${RuntimeVersion} -DCMAKE_BUILD_TYPE=RelWithDebInfo
+
+cd ..
 
 cmake --build ${SLBuildDirectory} --target install --config RelWithDebInfo
