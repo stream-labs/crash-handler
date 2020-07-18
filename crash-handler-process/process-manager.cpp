c8abd37b28acb515817f2733fcb0a1b00b8e9938
EddyGharbi <eddygharbiema@gmail.com>
2020-07-17 17:10:06 -0700

    Reduce monitoring interval

--- crash-handler-process/process-manager.cpp
+++ crash-handler-process/process-manager.cpp
@@ -112,7 +112,7 @@ void ProcessManager::monitor_fnc() {
                 unresponsiveMarked = true;
             }
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
 
     this->watcher->stop = true;
