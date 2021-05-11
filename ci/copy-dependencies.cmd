61123a76e642c4a070607f4a73cece845d2ef22e
EddyGharbi <eddygharbiema@gmail.com>
2021-05-11 14:57:45 -0700

    fix deps path

--- ci/copy-dependencies.cmd
+++ ci/copy-dependencies.cmd
@@ -1,6 +1,6 @@
-copy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x64\Microsoft.VC140.CRT\msvcp140.dll" "%FullDistributePath%\"
-copy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x64\Microsoft.VC140.CRT\vcruntime140.dll" "%FullDistributePath%\"
-copy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x64\Microsoft.VC140.CRT\vcruntime140_1.dll" "%FullDistributePath%\"
+copy "C:\Windows\System32\msvcp140.dll" "%FullDistributePath%\"
+copy "C:\Windows\System32\vcruntime140.dll" "%FullDistributePath%\"
+copy "C:\Windows\System32\vcruntime140_1.dll" "%FullDistributePath%\"
 
 copy "C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x64\api-ms-win-crt-heap-l1-1-0.dll" "%FullDistributePath%\"
 copy "C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x64\api-ms-win-crt-multibyte-l1-1-0.dll" "%FullDistributePath%\"
