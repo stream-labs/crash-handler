f7714070c48d9e4f9234ba1caf5f036fcb7f6b27
EddyGharbi <eddygharbiema@gmail.com>
2021-05-11 14:44:16 -0700

    Copy dependencies

---
+++ ci/copy-dependencies.cmd
@@ -0,0 +1,8 @@
+copy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x64\Microsoft.VC140.CRT\msvcp140.dll" "%FullDistributePath%\"
+copy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x64\Microsoft.VC140.CRT\vcruntime140.dll" "%FullDistributePath%\"
+copy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x64\Microsoft.VC140.CRT\vcruntime140_1.dll" "%FullDistributePath%\"
+
+copy "C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x64\api-ms-win-crt-heap-l1-1-0.dll" "%FullDistributePath%\"
+copy "C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x64\api-ms-win-crt-multibyte-l1-1-0.dll" "%FullDistributePath%\"
+copy "C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x64\api-ms-win-crt-runtime-l1-1-0.dll" "%FullDistributePath%\"
+copy "C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x64\api-ms-win-crt-string-l1-1-0.dll" "%FullDistributePath%\"
