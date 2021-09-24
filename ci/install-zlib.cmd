97ae612aef4105e5db4f7248018d82bf7bdab4e3
unknown <stevensoftware52@gmail.com>
2021-09-23 17:15:07 -0700

    testing pipeline

--- ci/install-zlib.cmd
+++ ci/install-zlib.cmd
@@ -4,3 +4,5 @@ set DepsURL=https://obs-studio-deployment.s3-us-west-2.amazonaws.com/%DEPS%.zip
 if exist %DEPS%.zip (curl -kLO %DepsURL% -f --retry 5 -z %DEPS%.zip) else (curl -kLO %DepsURL% -f --retry 5 -C -)
 
 7z x %DEPS%.zip -aoa -o%DEPS%
+
+setx DepsPath %DEPS%\win64
