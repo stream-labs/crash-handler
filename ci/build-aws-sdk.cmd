40dfacb67e8cbda5ae31b9468be135300a1a7a6a
Vladimir Sumarov <summeroff@gmail.com>
2022-06-16 16:56:07 -0700

    add aws sdk caching

---
+++ ci/build-aws-sdk.cmd
@@ -0,0 +1,12 @@
+mkdir aws-sdk
+cd aws-sdk
+
+git clone --branch %AWS_SDK_VERSION% --depth 1 --recurse-submodules git@github.com:aws/aws-sdk-cpp.git
+
+mkdir build
+cd build
+
+cmake -G "Visual Studio 17 2022" -A x64  -DBUILD_SHARED_LIBS=OFF -DBUILD_ONLY="s3;sts" -DENABLE_TESTING=OFF -DCUSTOM_MEMORY_MANAGEMENT=OFF -DCMAKE_INSTALL_PREFIX=%cd%../awsi ../aws-sdk-cpp
+
+cmake --build . --target install --config RelWithDebInfo
+
