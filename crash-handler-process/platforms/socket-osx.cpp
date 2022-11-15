61e713368341b30fb9e825f1f5945c9e1579b9fa
Eugen <97041180+eugen15@users.noreply.github.com>
2022-11-15 11:30:39 -0500

    Fixed macOS blocking open which prevented process exit

--- crash-handler-process/platforms/socket-osx.cpp
+++ crash-handler-process/platforms/socket-osx.cpp
@@ -28,16 +28,40 @@ std::unique_ptr<Socket> Socket::create()
 
 std::vector<char> Socket_OSX::read()
 {
+	// Previously, |open| blocked if there was not any data.
+	// So the process did not exit when it had to.
+	// The workaround is O_NONBLOCK + select + 500 ms timeout.
+	// It emulates the Windows implementation behavior.
+
 	std::vector<char> buffer;
-	buffer.resize(30000, 0);
-	int file_descriptor = open(this->name.c_str(), O_RDONLY);
+	int file_descriptor = open(this->name.c_str(), O_RDONLY|O_NONBLOCK);
 	if (file_descriptor < 0) {
-		log_info << "Could not open " << strerror(errno) << std::endl;
+		log_info << "Could not open; |open| error: " << strerror(errno) << std::endl;
 		return buffer;
 	}
+
+	fd_set set;
+	FD_ZERO(&set);
+	FD_SET(file_descriptor, &set);
+
+	struct timeval timeout;
+	timeout.tv_sec = 0;
+	timeout.tv_usec = 500000; // 500 ms
+
+	int rv = select(file_descriptor + 1, &set, NULL, NULL, &timeout);
+	if (rv <= 0) {
+		if (rv < 0) {
+			log_info << "Could not open; |select| error: " << strerror(errno) << std::endl;
+		}
+		close(file_descriptor);
+		return buffer;
+	}
+
+	buffer.resize(30000, 0);
 	int bytes_read = ::read(file_descriptor, buffer.data(), buffer.size());
 	buffer.resize(bytes_read < 0 ? 0 : bytes_read);
 	close(file_descriptor);
+
 	return buffer;
 }
 
